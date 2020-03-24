81fd60fd310867cb3bb58d9d099814378b2c9936
Ian Livingstone <ianl@cs.dal.ca>
2020-03-24 17:11:56 -0300

    Simplify error output to command line
    Previously, we were echoing out the stringified cause value which was
    unnecessary and ugly in the command line output.
    
    In future we could add a global verbose flag that _could_ bring it back
    if we wanted.

--- cmd/utils.go
+++ cmd/utils.go
@@ -4,13 +4,26 @@ import (
 	"crypto/rand"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/manifoldco/go-base32"
 	"github.com/urfave/cli/v2"
+
+	errors "github.com/dropoutlabs/cape/partyerrors"
 )
 
 func errorPrinter(err error) {
-	fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	msg := ""
+	switch e := err.(type) {
+	case *errors.Error:
+		// We don't want to display the cause string to the user, it's not
+		// important and just adds clutter at this level of display.
+		msg = strings.Join(e.Messages, ", ")
+	default:
+		msg = err.Error()
+	}
+
+	fmt.Fprintf(os.Stderr, "\nError: %s\n", msg)
 	os.Exit(1)
 }
 
