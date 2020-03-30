ee7636a682e9560037661729a90a2e2a0d6be762
Ben DeCoste <bendecoste@gmail.com>
2020-03-30 11:03:02 -0300

    Implement component in connector

--- connector/connector.go
+++ connector/connector.go
@@ -1,7 +1,11 @@
 package connector
 
 import (
+	"context"
 	"fmt"
+	"github.com/justinas/alice"
+	"github.com/manifoldco/healthz"
+	"net/http"
 	"time"
 
 	"github.com/dropoutlabs/cape/auth"
@@ -13,12 +17,17 @@ type Connector struct {
 	InstanceID string
 	Port       int
 	Token      *auth.APIToken
+	handler    http.Handler
 }
 
 // Start the connector
-func (c *Connector) Start() error {
+func (c *Connector) Setup(ctx context.Context) (http.Handler, error) {
 	time.Sleep(5 * time.Minute)
 
+	return c.handler, nil
+}
+
+func (c *Connector) Teardown(ctx context.Context) error {
 	return nil
 }
 
@@ -28,9 +37,15 @@ func New(cfg *Config) (*Connector, error) {
 		return nil, err
 	}
 
+	mux := http.NewServeMux()
+	health := healthz.NewHandler(mux)
+
+	chain := alice.New().Then(health)
+
 	return &Connector{
 		InstanceID: fmt.Sprintf("cape-connector-%s", cfg.InstanceID),
 		Port:       cfg.Port,
 		Token:      cfg.Token,
+		handler:    chain,
 	}, nil
 }
