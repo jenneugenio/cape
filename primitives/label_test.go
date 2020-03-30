f502a9f34bd8649a99e398c0f55f9d0fd848e380
Ian Livingstone <ianl@cs.dal.ca>
2020-03-30 11:00:01 -0300

    Fix bug: label not supporting base32
    There isn't a reason for our Label to not support base32 so now it does!
    If a user wants to add a zero or any other number to their labels we
    should support it!

---
+++ primitives/label_test.go
@@ -0,0 +1,62 @@
+package primitives
+
+import (
+	"testing"
+
+	gm "github.com/onsi/gomega"
+
+	errors "github.com/dropoutlabs/cape/partyerrors"
+)
+
+func TestLabel(t *testing.T) {
+	gm.RegisterTestingT(t)
+
+	tests := map[string]struct {
+		in    string
+		valid bool
+		cause errors.Cause
+	}{
+		"valid": {
+			in:    "assdf049",
+			valid: true,
+		},
+		"too short": {
+			in:    "sd",
+			valid: false,
+			cause: InvalidLabelCause,
+		},
+		"too long": {
+			in:    "sdfsfsfsfasfsfdsdfsdfsdfsdfsdfsfsdfsdfsdfsdfsdfsdfsdfdsfsdfsdfdsfsdfsdfsdfdsfsdfsdfsdfsdfsfdsffsd",
+			valid: false,
+			cause: InvalidLabelCause,
+		},
+		"starts with a -": {
+			in:    "-sfsfsfsfsf",
+			valid: false,
+			cause: InvalidLabelCause,
+		},
+		"can start with a number": {
+			in:    "0sfasf",
+			valid: true,
+		},
+		"can contain - and numbers": {
+			in:    "sdfs-sdf0",
+			valid: true,
+		},
+		"cannot contain capitals": {
+			in:    "sdfdfsFDs",
+			valid: false,
+			cause: InvalidLabelCause,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			_, err := NewLabel(test.in)
+			gm.Expect(err == nil).To(gm.Equal(test.valid))
+			if test.valid {
+				gm.Expect(errors.FromCause(err, test.cause))
+			}
+		})
+	}
+}
