c8f127fcfe583e72f0b6dbc2388f808b546137f3
Sean Swehla <sean@sweh.la>
2020-06-05 17:39:53 -0400

    Add support for paths to update command
    This makes the `update` command take an optional list of paths to search
    for database migrations to run on the configured db. This will allow us
    to bundle migrations for the databases associated with all supported
    services into a single container image or binary, and select the
    appropriate migrations at run time.

--- cmd/update.go
+++ cmd/update.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/urfave/cli/v2"
 
 	"github.com/capeprivacy/cape/coordinator/database"
@@ -9,7 +12,23 @@ import (
 
 func updateCmd(c *cli.Context) error {
 	dbURL := EnvVariables(c.Context, capeDBURL).(*primitives.DBURL)
-	migrator, err := database.NewMigrator(dbURL.URL, "coordinator/migrations")
+
+	migrations := c.Args().Slice()
+	if len(migrations) == 0 {
+		migrations = append(migrations, "coordinator/migrations")
+	}
+
+	for _, mPath := range migrations {
+		if _, err := os.Stat(mPath); err != nil {
+			if os.IsNotExist(err) {
+				return fmt.Errorf("path %q not found", mPath)
+			}
+
+			return fmt.Errorf("error reading path %q: %s", mPath, err.Error())
+		}
+	}
+
+	migrator, err := database.NewMigrator(dbURL.URL, migrations...)
 	if err != nil {
 		return err
 	}
@@ -22,8 +41,9 @@ func init() {
 		Usage:     "Update a running Cape coordinator to a new version",
 		Variables: []*EnvVar{capeDBURL},
 		Command: &cli.Command{
-			Name:   "update",
-			Action: updateCmd,
+			Name:      "update",
+			Action:    updateCmd,
+			ArgsUsage: "[ path/to/migrations [ path/to/more/migrations ... ]]",
 		},
 	}
 
