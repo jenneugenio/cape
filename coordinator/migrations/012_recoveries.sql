9da2431b6f291a8fcfaeb11b10fd348c289ce9ce
Ian Livingstone <ianl@cs.dal.ca>
2020-06-06 20:49:47 -0300

    Migration for Recoveries Table
    This commit introduces the up and down migration for the Recoveries
    table.

---
+++ coordinator/migrations/012_recoveries.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+CREATE TABLE recoveries (
+  id char(29) primary key not null,
+  user_id char(29) references identities(id) on delete cascade not null,
+  data jsonb not null,
+  CONSTRAINT recoveries_id_check CHECK (data::jsonb#>>'{id}' = id),
+  CONSTRAINT recoveries_user_id_check CHECK (data::jsonb#>>'{user_id}' = user_id)
+);
+
+CREATE TRIGGER recoveries_hoist_tgr
+  BEFORE INSERT ON recoveries
+  FOR EACH ROW EXECUTE PROCEDURE hoist_values('id', 'user_id');
+
+COMMIT;
+
+---- create above / drop below ----
+
+BEGIN;
+DROP TABLE recoveries;
+COMMIT;
