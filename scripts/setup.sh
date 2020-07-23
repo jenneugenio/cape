c08f9abdc7d72d24f96e27ac02933d462f0b0fee
Justin Patriquin <justinpatriquin@gmail.com>
2020-07-23 09:06:39 -0300

    enable setup to work for local deploy again

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,2 @@
-set -e
-set -x
-
-export CAPE_NAME=cape-user
-export CAPE_EMAIL=cape_user@mycape.com
-export CAPE_PASSWORD=capecape
-
-if ! cape setup local http://localhost:8080; then
-    cape config clusters remove -y local
-    cape setup local http://localhost:8080
-fi
+cape config clusters add local http://localhost:8080 || true
+CAPE_PASSWORD=capecape cape login --email cape_user@mycape.com
